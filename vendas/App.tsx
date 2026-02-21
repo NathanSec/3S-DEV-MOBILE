269b103b66f07c8d96c1bd4140a6c27b08bec656
NathanSec <nathanfsj.23@gmail.com>
2026-02-20 21:55:35 -0300

    Update 20/02

--- vendas/App.tsx
+++ vendas/App.tsx
@@ -1,20 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, ScrollView } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Bem Vindo(a) à página de vendas da nossa loja!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView style={styles.container}>
+      <Image source={require('./images/bubla.webp')} style={styles.imagem} resizeMode='contain' />
+      <Text style={styles.textStyle}>
+        Bulbasaur usou Chicote de Vinha e causou X de dano!{'\n'}
+        Squirtle usou Jato d'Água e causou Y de dano!{'\n'}
+        Bulbasaur venceu!</Text>
+      <StatusBar style="light" animated />
+      <Image source={require('./images/gifSquirtle.webp')} style={styles.imagem} resizeMode='contain' />
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'gray',
+  },
+  textStyle: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: 'white',
+    lineHeight: 24,
+    paddingVertical: 12,
+  },
+  imagem: {
+    height: 300,
+    backgroundColor: 'transparent',
+    alignSelf: 'center',
   },
 });
